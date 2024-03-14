a6d0f53f7c7db4afa865dc59d20e9304f7731c23
Soham Sinha <sohamsinha299@gmail.com>
2024-03-15 00:53:10 +0530

    edit

---
+++ daa/maxsubarray.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int maxsubarray(int x[], int size){
+  int maxsum=-INFINITY;
+  int continoussum=0;
+  for(int i=0; i<size; i++){
+    continoussum+=x[i];
+    if(x[i]>continoussum){
+      continoussum=x[i];
+    }
+    if(maxsum<continoussum){
+      maxsum=continoussum;
+      }
+    
+  }
+  return maxsum;
+}
+
+int main(){
+  int n[]={2, -4, 1, 9, -6, 7, -3};
+  int s= sizeof(n)/sizeof(n[1]);
+  cout<<maxsubarray(n, s);
+}
